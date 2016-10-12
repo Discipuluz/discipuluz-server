e4244acb152b82b12083312a7818b8d3d204e6e3
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-10-12 08:14:32 -0300

    Added nginx volume web deploy.

--- deploy/web.sh
+++ deploy/web.sh
@@ -4,4 +4,4 @@ docker stop web
 docker rm web
 docker rmi quay.io/discipuluz/discipuluz-web:current
 docker tag quay.io/discipuluz/discipuluz-web:$1 quay.io/discipuluz/discipuluz-web:current
-docker run -d --name="web" --restart="on-failure" --publish="80:80" --publish="443:443" --link="back:back" quay.io/discipuluz/discipuluz-web:$1
+docker run -d --name="web" --restart="on-failure" --publish="80:80" --publish="443:443" --link="back:back" --volume="/etc/nginx/nginx.conf:/etc/nginx/nginx.conf" quay.io/discipuluz/discipuluz-web:$1
