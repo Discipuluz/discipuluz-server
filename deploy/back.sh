e8c5e09ec50ad4bb61196c5026a73b8732db55db
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2016-10-16 18:37:27 -0200

    Fixed back's python buffer.

--- deploy/back.sh
+++ deploy/back.sh
@@ -4,4 +4,4 @@ docker stop back
 docker rm back
 docker rmi quay.io/discipuluz/discipuluz-back:current
 docker tag quay.io/discipuluz/discipuluz-back:$1 quay.io/discipuluz/discipuluz-back:current
-docker run -d --name="back" --restart="on-failure" --link="db:db" quay.io/discipuluz/discipuluz-back:$1
+docker run -d --name="back" --restart="on-failure" --link="db:db" -env="PYTHONUNBUFFERED=0" quay.io/discipuluz/discipuluz-back:$1
